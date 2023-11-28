35ccb8124c192645141c49e1095a5bb6c280857c
Jhon Dany Mamani Canahuire <jmamanic@uandina.edu.pe>
2023-11-28 17:27:45 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo_sp/pages/home_pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Shared Preferences App",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
